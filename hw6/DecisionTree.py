368ad888f383429e8e553022d07c6ea1929bfded
Spencer Barton <sbarton272@gmail.com>
2014-02-17 00:50:37 -0500

    wrote generate children and associated helper functions, not tested

--- hw6/DecisionTree.py
+++ hw6/DecisionTree.py
@@ -3,6 +3,8 @@
 # HW 6 2/19/14
 # DecisionTree.py
 
+import math, sys, csv
+
 class DecisionTree(object):
 	"""DecisionTree for performing algorithm"""
 	def __init__(self, arg):
@@ -16,13 +18,16 @@ class DecisionTree(object):
 
 class Node(object):
 	"""Nodes that make-up tree. Has children or is a leaf."""
-	def __init__(self, depth, examples, targetPlus, targetMinus, minEntropy):
+	def __init__(self, depth, examples, targetPlus, targetMinus, minEntropy, 
+				 targetKey = "Target", attrKey = "Attrs"):
 		# constructor args
 		self.depth 			= depth 		# int
-		self.examples 		= examples 		# dict
+		self.examples 		= examples 		# list of dict: {Attrs: {Attr: val, ... } , Target: (+,-) }
 		self.targetPlus 	= targetPlus 	# label
 		self.targetMinus 	= targetMinus 	# label
 		self.minEntropy		= minEntropy 	# float, lowest Entropy to branch on
+		self.targetKey		= targetKey		# for access into examples
+		self.attrKey		= attrKey		# for access into examples
 		
 		# examples meta data
 		self.nPlus  = 0
@@ -39,31 +44,86 @@ class Node(object):
 	# Generating children and helpers
 	#=================================================================
 
-	def _entropy(self):
+	def _entropy(self, examples):
 		"""calculate entropy of examples: H(Y)
 			returns float"""
-		pass
+		nElems = float(len(examples))
+		nPlusElems  = sum(1 for e in examples if (e[self.targetKey] == self.targetPlus) )
+		nMinusElems = nElems - nPlusElems
+		probP = nPlusElems / nElems
+		probM = nMinusElems / nElems
+		return -probP * math.log(probP, 2) - probM * math.log(probM, 2)
 
 	def _condEntropy(self, attr):
 		"""calculate conditional entropy of examples: H(Y|A)
 			returns float"""
-		pass
+		nElems = float(len(self.examples))
 
+		splitExamples = self._split(attr)
+
+		# calculate entropy conditioned on attr values
+		condEntropy = 0.0
+		for val in splitExamples:
+			probVal = len(splitExamples[val]) / nElems
+			entropyCondOnVal = self._entropy(splitExamples[val])
+			condEntropy += probVal * entropyCondOnVal
+
+		return condEntropy
 
 	def _mutualInfo(self, attr):
 		"""determine the mutual information between the examples and the given attribute.
 			I(Y;A) = H(Y) - H(Y|A) where Y is examples (data) and A is attribute. 
 			returns float"""
-		pass
+		return self._entropy(self.examples) - self.condEntropy(attr)
 
 	def _split(self, attr):
 		"""split the examples on an attribute's values (v1, v2, ...)
 			returns {v1 : examples_v1, v2 : examples_v2, ...} """
-		pass
+		# split examples by attr values
+		splitExamples = {}
+		for ex in self.examples:
+			val = ex[self.attrKey][attr]
+			# add val as key in split examples
+			if val not in splitExamples:
+				splitExamples[val] = []
+			# add example to split
+			splitExamples[val].append(ex)
+
+		return splitExamples
 
 	def _generateChildren(self):
-		"generate child nodes based on maximizing information gain"
-		pass
+		"""generate child nodes based on maximizing information gain.
+			Returns (splitAttr, children) whose values can be None if no split"""
+		# no children generated if at max depth or no more training examples or no more attributes
+		if ( (depth == 0) or (len(self.examples) == 0) or 
+			 (len(self.examples[0][self.attrKey]) == 0) ):
+			return (None, None)
+
+		# find attribute to branch on
+		maxMutualInfo = 0.0 # mutual info is pos for these cases
+		maxAttr = None
+		for attr in self.examples[0][self.attrKey]:
+			mutualInfo = self._mutualInfo(attr)
+			# update maximum
+			if mutualInfo >= maxMutualInfo:
+				maxMutualInfo = mutualInfo
+				maxAttr = attr
+		splitAttr = maxAttr
+
+		# if max entropy is not larger then minEntropy then don't branch
+		if maxMutualInfo < self.minEntropy:
+			return (None, None)
+
+		# split on attr values and return new examples
+		examplesByVal = self._split(splitAttr)
+		# generate children
+		children = {}
+		for val in examplesByVal:
+			examples = examplesByVal[val]
+			children[val] = Node(self.depth - 1, examples, self.targetPlus, 
+								 self.targetMinus, self.minEntropy)
+		
+		return (splitAttr, children)
 
 	#=================================================================
 	# Other methods
