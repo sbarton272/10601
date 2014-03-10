cd5fcc50ee596f79c1a5d887c50a4d1a246e2e59
Spencer Barton <sbarton272@gmail.com>
2014-03-09 22:57:09 -0400

    Started layer class for further abstracton, working on ANN class

---
+++ hw8/ANNNodeLayer.m
@@ -0,0 +1,36 @@
+classdef ANNNodeLayer
+    % ANNNodeLayer Layer of nodes
+    % Keeps track of layer size and calculates output
+    
+    properties
+        layerSize;
+        nodes;
+    end
+    
+    methods
+        function obj = ANNNodeLayer(layerSize, nNodeInputs, nodeThreshFunct)
+            if (nargin > 0)
+                obj.layerSize = layerSize;
+                
+                % init backwards important for pre allocation
+                for n = obj.layerSize:-1:1
+                    obj.nodes(1,n) = ANNNode(nNodeInputs, nodeThreshFunct);
+                end
+            end
+        end % ANNNodeLayer
+        
+        function output = getLayerOutput(input)
+       
+            % prealloc
+            output = 1:obj.layerSize;
+            % loop is more efficient then arrayfun
+            for n = 1:obj.layerSize
+                output(n) = obj.nodes(n).getOutput(input);
+            end
+            
+        end % getLayerOutput
+        
+    end
+    
+end
+
--- hw8/ArtificialNeuralNetwork.m
+++ hw8/ArtificialNeuralNetwork.m
@@ -8,18 +8,22 @@ classdef ArtificialNeuralNetwork
       TrainingAlg = BackPropegation; % TODO implement
         
       % size parameters
-      nHiddenLayers = 1;
-      nHiddenNodes = 1;
+      nLayers = 2; % includes output layer
+      nHiddenNodes = 4;
       nInputs = 1;
       nOutputs = 1;
       
       % layers of Nodes
-      hiddenLayers;
-      outputLayer;
+      % output layer is the top layer at index 1
+      % hidden layers are ordered from layer 2 down
+      layers;
       
       % Various network functions
       nodeThreshFunct = sigmoid;
       outputThreshFunct = @(x) x; %identity function
+      
+      % flags
+      bTrained = false;
       
    end
    
@@ -30,7 +34,7 @@ classdef ArtificialNeuralNetwork
           % initialize ANN
           if (nargin > 0)
               obj.TrainingAlg = TrainingAlg;
-              obj.nHiddenLayers = nHiddenLayers;
+              obj.nLayers = nHiddenLayers;
               obj.nHiddenNodes = nHiddenNodes;
               obj.nInputs = nInputs;
               obj.nOutputs = nOutputs;
@@ -38,33 +42,50 @@ classdef ArtificialNeuralNetwork
               obj.outputThreshFunct = outputThreshFunct;
           end
           
-          % init layers vectors
+          % output layer allocation
           % init backwards important for pre allocation
-          for n = obj.nOutputs:-1:1
-            obj.outputLayer(1,n) = ANNNode(obj.nHiddenNodes, ...
-                                           obj.nodeThreshFunct);
-          end
-          
-          % hidden layers
-          % TODO finish this
-          for n = obj.nHiddenNodes:-1:1
-            obj.hiddenLayers(1,n) = ANNNode(obj.nHiddenNodes, ...
-                                           obj.nodeThreshFunct);
+          obj.layers = 1:obj.nLayers; % pre alloc
+          % output layer
+          obj.layers(1,1) = ANNNodeLayer(obj.nOutputs, obj.nHiddenNodes,...
+                                         obj.nodeThreshFunct);
+          % init bottom hidden layer
+          obj.layers(1,obj.nLayers) = ANNNodeLayer(obj.nHiddenNodes, ...
+                                                   obj.nInputs,...
+                                                   obj.nodeThreshFunct);          
+          % init internal hidden layers
+          for layerN = 2:(obj.nLayers-1)
+            obj.layers(1,layerN) = ANNNodeLayer(obj.nHiddenNodes, ...
+                                                obj.nHiddenNodes,...
+                                                obj.nodeThreshFunct);
           end
           
       end % ArtificialNeuralNetwork
       
       function output = getOutput(input)
+          % getOutput Get the output vector of a trained ANN given an input
+          % vector
           assert( isEqaul( size(inputs), [1, obj.nInputs] ), ...
             ['ArtificialNeuralNetwork.getOutput:', ...
             'Inputs must be vector of correct size']);
-          output = 1:obj.nOutputs; % prealloc
-          % TODO finish
+          
+          output = 1:obj.nOutputs; % prealloc output
+          % propogate input through the layers
+          for layerN = 1:obj.nHiddenLayers
+              
+          end
+          
           
       end % getOutput
       
+      function output = getLayerOutput(input, layerN)
+          % getLayerOutput Get the output of a given layer given the input
+          output = 1:obj.nInputs; % TODO generalize with layer object
+          nodes = hiddenLayers()
+      end % getLayerOutput
+      
       function train()
           obj.TrainingAlg.train();
+          obj.bTrained = true;
       end % train
       
       function y = sigmoid(x)
